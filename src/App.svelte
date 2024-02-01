807e2296909d8454f2d34f6d92bcad4e92054162
Nari Johnson <nari@MacBook-Pro-Nari.local>
2024-01-31 20:52:26 -0500

    redesign

--- src/App.svelte
+++ src/App.svelte
@@ -6,111 +6,108 @@
 
 <main>
 	<div id = "page-container">
-		<div id="header"><b>Nari Johnson</b></div>
-		<div id = "links"><a href = "https://scholar.google.com/citations?hl=en&user=otKyVIAAAAAJ">Google Scholar</a> <img src = {pagebreak_fp} width = "20"/> <a href = "https://github.com/njohnson99">Github</a> <img src = {pagebreak_fp} width = "20"/> <a href = "https://twitter.com/narijohnson">Twitter</a> </div>
-
-		<div id="intro-div">
-			Hello! My name is Nari (rhymes with "<a href = {starrynight}>starry</a>") and I am a PhD student in Carnegie Mellon University's <a href = "https://www.ml.cmu.edu/about/">Machine Learning Department</a> advised by <a href = "https://www.cs.cmu.edu/~hheidari/">Hoda Heidari</a>.
-			I graduated from Harvard in 2021 with a BA and MS in Computer Science, where I previously worked with <a href = "https://finale.seas.harvard.edu">Finale Doshi-Velez</a>.<br><br>
-			
-			I am broadly interested in understanding the societal impacts of artificial intelligence (AI) technologies, and how such technologies are (or might be) governed.
-			My recent work studies tools and processes that help stakeholders imagine, anticipate, and take action to mitigate potential harms of AI. 
-			My present research interests lie in AI transparency, evaluation, and accountability.
-			<br><br>
-
-			Email: narij at andrew dot cmu dot edu 
+		<div id="page-top">
+			<div class="header"><b>Nari Johnson</b></div>
+			<div id = "links"><a href = "https://scholar.google.com/citations?hl=en&user=otKyVIAAAAAJ">Google Scholar</a> 🌼 <a href = "https://github.com/njohnson99">Github</a> 🌼 <a href = "https://twitter.com/narijohnson">Twitter</a> </div>
 		</div>
+		<div class="container">
+
+			<div id="intro-div">
+				Hello! My name is Nari (rhymes with "<a href = {starrynight}>starry</a>") and I am a third-year PhD student in Carnegie Mellon University's <a href = "https://www.ml.cmu.edu/about/">Machine Learning Department</a>, where I'm fortunate to be advised by <a href = "https://www.cs.cmu.edu/~hheidari/">Hoda Heidari</a>.
+				I graduated from Harvard in 2021 with a BA and MS in Computer Science, where I previously worked with <a href = "https://finale.seas.harvard.edu">Finale Doshi-Velez</a>.
+				My present research interests lie in AI transparency, evaluation, and accountability.
+				<br><br>
+
+				<b>Email: narij at andrew dot cmu dot edu </b>
+			</div>
+			<div class="headshot-div">
+				<img class="circle" src="./imgs/headshot.JPG" alt="A photo of Nari sitting at a cafe."/>
+			</div>
+		</div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 
 		<div id = "conf-div">
-			<div id = "header">Refereed Publications (peer reviewed)</div>
+			<div class="header-two"><b>Refereed Publications</b></div>
 
 			* denotes equal contribution.
 			
 			<div class = "article">
-				<i>"Where Does My Model Underperform? A Human Evaluation of Slice Discovery Algorithms"</i>. 
-				<b>Nari Johnson</b>, Ángel Alexander Cabrera, Gregory Plumb, Ameet Talwalkar.
-				ICML Workshop on Spurious Correlations, Invariance, and Stability, 2023.
-				AAAI Conference on Human Computation and Crowdsourcing (HCOMP), 2023.
+				<b>Where Does My Model Underperform? A Human Evaluation of Slice Discovery Algorithms</b><br> 
+				<b>Nari Johnson</b>, Ángel Alexander Cabrera, Gregory Plumb, Ameet Talwalkar<br>
+				AAAI Conference on Human Computation and Crowdsourcing (HCOMP), 2023<br>
+				ICML Workshop on Spurious Correlations, Invariance, and Stability, 2023<br>
 				[<a href = "https://arxiv.org/abs/2306.08167">arXiv</a>] [<a href = "https://youtu.be/ObRWhgHJLUI?t=28">conference talk</a>]<br>
-				<div class = "honors-div">
-					HCOMP Best Paper Award (top 1) <br>
-					Spotlight Presentation (top 5%) at the ICML SCIS Workshop
-				</div>
-				
+				🌟 HCOMP 2023 Best Paper Award (top 1) <br>
+				🌟 Spotlight Presentation (top 5%) at the ICML SCIS Workshop
 			</div>
 
 			<div class = "article">
-				<i>"Towards a More Rigorous Science of Blindspot Discovery in Image Classification Models"</i>. 
-				Gregory Plumb*, <b>Nari Johnson</b>*, Ángel Alexander Cabrera, Ameet Talwalkar.
-				ICML Workshop on Spurious Correlations, Invariance, and Stability, 2022.
-				Transactions on ML Research (TMLR), 2023.
+				<b>Towards a More Rigorous Science of Blindspot Discovery in Image Classification Models</b><br> 
+				Gregory Plumb*, <b>Nari Johnson</b>*, Ángel Alexander Cabrera, Ameet Talwalkar<br>
+				Transactions on ML Research (TMLR), 2023<br>
+				ICML Workshop on Spurious Correlations, Invariance, and Stability, 2022<br>
 				[<a href = "https://arxiv.org/abs/2207.04104">arXiv</a>] [<a href = "https://github.com/HazyResearch/domino/blob/main/domino/_slice/planespot.py">code: PlaneSpot</a>] [<a href = "https://github.com/njohnson99/spotcheck">code: SpotCheck</a>]
 			</div>
 
 
 			<div class = "article">
-				<i>"Use-Case-Grounded Simulations for Explanation Evaluation"</i>. 
-				Valerie Chen, <b>Nari Johnson</b>, Nicholay Topin*, Gregory Plumb*, Ameet Talwalkar.
-				NeurIPS, 2022.
+				<b>Use-Case-Grounded Simulations for Explanation Evaluation</b><br> 
+				Valerie Chen, <b>Nari Johnson</b>, Nicholay Topin*, Gregory Plumb*, Ameet Talwalkar<br>
+				NeurIPS, 2022<br>
 				[<a href = "https://arxiv.org/abs/2206.02256">arXiv</a>] [<a href = "https://github.com/valeriechen/simeval_tutorial/tree/main">code</a>]
 			</div>
 
 
 			<div class = "article">
-				<i>"OpenXAI: Towards a Transparent Evaluation of Model Explanations"</i>. 
-				Chirag Agarwal, Satyapriya Krishna, Eshika Saxena, Martin Pawelczyk, <b>Nari Johnson</b>, Isha Puri, Marinka Zitnik, Himabindu Lakkaraju.
-				ICLR Pair2Struct Workshop, 2022.
-				NeurIPS Track on Datasets and Benchmarks, 2022.
+				<b>OpenXAI: Towards a Transparent Evaluation of Model Explanations</b><br>
+				Chirag Agarwal, Satyapriya Krishna, Eshika Saxena, Martin Pawelczyk, <b>Nari Johnson</b>, Isha Puri, Marinka Zitnik, Himabindu Lakkaraju<br>
+				NeurIPS Track on Datasets and Benchmarks, 2022<br>
+				ICLR Pair2Struct Workshop, 2022<br>
 				[<a href = "https://arxiv.org/abs/2206.11104">arXiv</a>] [<a href = "https://open-xai.github.io">code</a>] <br>
-				<div class = "honors-div">
-					Oral Presentation (top 5%) at the ICLR Pair2Struct Workshop
-				</div>
+				🌟 Oral Presentation (top 5%) at the ICLR Pair2Struct Workshop
 			</div>
 
 			<div class = "article">
-				<i>"Learning Predictive and Interpretable Timeseries Summaries from ICU Data"</i>.
-				<b>Nari Johnson</b>, Sonali Parbhoo, Andrew Slavin Ross, Finale Doshi-Velez.
-				AMIA Annual Symposium, 2021.  [<a href = "https://arxiv.org/abs/2109.11043">arXiv</a>]  <br>
-				<div class = "honors-div">
-					AMIA Student Paper Competition Finalist (top 8)<br>
-					Knowledge Discovery & Data Mining Student Innovation Award (2 awarded)
-
-				</div>
-				
+				<b>Learning Predictive and Interpretable Timeseries Summaries from ICU Data</b><br>
+				<b>Nari Johnson</b>, Sonali Parbhoo, Andrew Slavin Ross, Finale Doshi-Velez<br>
+				AMIA Annual Symposium, 2021<br>
+				[<a href = "https://arxiv.org/abs/2109.11043">arXiv</a>]<br>
+				🌟 AMIA Student Paper Competition Finalist (top 8)<br>
+				🌟 Knowledge Discovery & Data Mining Student Innovation Award		
 			</div>
 
 		</div>
 		<div id = "conf-div">
-			<div id = "header">Workshop Papers & Pre-prints (lightly peer reviewed)</div>
-			<i>"Assessing AI Impact Assessments: A Classroom Study"</i>. 
-			<b>Nari Johnson</b>, Hoda Heidari.
-			NeurIPS Regulatable ML Workshop, 2023.
+			<div class= "header-two"><b>Workshop Papers & Pre-prints</b></div>
+			<b>Assessing AI Impact Assessments: A Classroom Study</b><br> 
+			<b>Nari Johnson</b>, Hoda Heidari<br>
+			NeurIPS Regulatable ML Workshop, 2023<br>
 			[<a href = "https://arxiv.org/abs/2311.11193">arXiv</a>] 
 
 		</div>
 
 		<div id = "teaching-div">
-			<div id = "header">Teaching</div>
+			<div class = "header-two"><b>Teaching</b></div>
 			
 			I've greatly enjoyed teaching several classes in college and my PhD.
 			As an undergrad, I designed and led Harvard Computer Science's first <a href = "https://www.thecrimson.com/article/2020/10/7/cs-tf-training/">inclusive teaching training</a>.
 			My teaching has been recognized with several honors, including Carnegie Mellon's <strong>Machine Learning TA Award</strong>, Harvard's <strong><a href = "https://csadvising.seas.harvard.edu/opportunities/patel/">Alex Patel Teaching Fellowship</a></strong>, and Harvard's <strong><a href = "https://bokcenter.harvard.edu/teaching-awards">Derek Bok Certificate of Distinction in Teaching</a> (4x)</strong>.<br>
 
 			<ul>
-				<li><a href = "https://www.cs.cmu.edu/~aarti/Class/10701_Spring23/">10-701: Introduction to Machine Learning</a>, Spring 2023</li>
-				<li><a href = "https://harvard-ml-courses.github.io/cs181-web/">CS 181: Machine Learning</a>, Spring 2021. <b>Head Teaching Fellow</b></li>
-				<li><a href = "http://people.seas.harvard.edu/~madhusudan/courses/Fall2020/">CS 121: Theoretical Computer Science</a>, Fall 2020. <b>Alex Patel Fellow</b></li>
-				<li><a href = "https://harvard-ml-courses.github.io/cs181-web/">CS 181: Machine Learning</a>, Spring 2020</li>
-				<li><a href = "https://cs121.boazbarak.org/">CS 121: Theoretical Computer Science</a>, Fall 2019</li>
-				<li><a href = "https://qrd.college.harvard.edu/classes/math-23c-mathematics-computation-statistics-and-data-science">Math 23c: Mathematics for Computation and Data Science</a>, Spring 2019</li>
+				<li><a href = "https://www.cs.cmu.edu/~hheidari/rai-spring-24.html">CMU, 10-735: Responsible AI</a>, Spring 2024</li>
+				<li><a href = "https://www.cs.cmu.edu/~aarti/Class/10701_Spring23/">CMU, 10-701: Introduction to Machine Learning</a>, Spring 2023</li>
+				<li><a href = "https://harvard-ml-courses.github.io/cs181-web/">Harvard, CS 181: Machine Learning</a>, Spring 2021. <b>Head Teaching Fellow</b></li>
+				<li><a href = "http://people.seas.harvard.edu/~madhusudan/courses/Fall2020/">Harvard, CS 121: Theoretical Computer Science</a>, Fall 2020. <b>Alex Patel Fellow</b></li>
+				<li><a href = "https://harvard-ml-courses.github.io/cs181-web/">Harvard, CS 181: Machine Learning</a>, Spring 2020</li>
+				<li><a href = "https://cs121.boazbarak.org/">Harvard, CS 121: Theoretical Computer Science</a>, Fall 2019</li>
+				<li><a href = "https://qrd.college.harvard.edu/classes/math-23c-mathematics-computation-statistics-and-data-science">Harvard, Math 23c: Mathematics for Computation and Data Science</a>, Spring 2019</li>
 			</ul>
 		</div>
 
 		<div id = "service-div">
-			<div id = "header">Service</div>
+			<div class = "header-two"><b>Service</b></div>
 
 			I care deeply about creating supportive communities in computing.
-			At CMU, I've participated in <a href = "https://www.cmu.edu/scs/technights/">TechNights</a> and the AI Mentorship Program.
+			At CMU, I organize my department's Wellness Committee and plan community-building events for women and other under-represented gender minorities. I also participated in <a href = "https://www.cmu.edu/scs/technights/">TechNights</a> and the AI Mentorship Program.
 			In a past life, I served as elected Co-President and led the Advocacy Council of <a href = "https://www.harvardwics.com">Harvard Women in Computer Science</a>.<br><br>
 
 			<b>Professional Service</b>:  I've served as a reviewer for: ICLR 2024, NeurIPS 2023, CHI LBW 2023.<br>
@@ -127,41 +124,64 @@
 
 
 @font-face {
-	font-family: 'Editorial';
-	src: local('PP Editorial New'), url('./fonts/PPEditorialNew-Regular.woff') format('woff');
+	font-family: 'Lato';
+	src: local('Lato Light'), url('./fonts/Lato-Light.tff') format('tff');
 	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 	font-weight: bold;
 }
+
 @font-face {
-	font-family: 'Editorial';
-	src: local('PP Editorial New'), url('./fonts/PPEditorialNew-UltralightItalic.woff') format('woff');
+	font-family: 'Lato';
+	src: local('Lato Regular'), url('./fonts/Lato-Regular.tff') format('tff');
 	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-	font-style: italic;
+	font-weight: bold;
 }
-@font-face {
-	font-family: 'Editorial';
-	src: local('PP Editorial New'), local('Editorial-Ultralight'), url('./fonts/PPEditorialNew-Ultralight.woff') format('woff');
-	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+
+.container {
+	display: flex;
+	align-items: top;
+	column-gap: 20px;
+}
+
+#page-top{
+	text-align: center;
+}
+.circle{
+	border-radius: 50%; 
+}
+img {   
+    height: auto;
+    width: 100%;
+    max-width: 700px;
+	min-width: 250px;
 }
 
 #page-container { 
 	margin-top: 2cm;
-    margin-left: 2cm;
-    margin-right: 2cm;
+    margin-left: 4cm;
+    margin-right: 4cm;
 	margin-bottom: 4cm;
-	font-size: 30px;
-	line-height: 40px;
-	font-family: 'Editorial', PP Editorial New;
+	font-size: 25px;
+	line-height: 35px;
+	font-family: 'Lato', Lato Light;
 }
 
 #links {
 	margin-bottom: 1cm;
 	font-size:25px;
 }
-#header{
+
+
+.header{
 	font-size: 40px;
 	margin-bottom: 10px;
-	font-family: 'Editorial', PP Editorial New;
+	font-family: 'Lato', Lato Regular;
+}
+
+.header-two{
+	font-size: 35px;
+	margin-bottom: 10px;
+	font-family: 'Lato', Lato Regular;
 }
 
 #intro-div {
@@ -174,7 +194,7 @@
 }
 
 #conf-div {
-	font-size: 25px;
+	font-size: 23px;
 	margin-bottom: 1cm;
 }
 
@@ -189,7 +209,7 @@
 }
 
 mark {
-	background-color: #ece1fd;
+	background-color: #df2082;
 }
 
 #teaching-div {
